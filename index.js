2c9249ccc9d8f6c79131c5459f9b549f2fde9d3b
Okin-PC <201130339@student.uj.ac.za>
2022-04-10 22:37:23 +0200

    added registration number and price data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const results = document.querySelector(".results");
 const generateButton = document.getElementById("generate")
 const makeValue = document.getElementById("make");
 const colorValue = document.getElementById("color");
+const reg_numberValue = document.getElementById("RegNum");
+const priceValue = document.getElementById("price");
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -34,8 +36,25 @@ const filterByMake = (make) => {
             const res = resultsTemplate({"results": response.data});
             results.innerHTML = res
         });
-
 }
+
+const filterByPrice = (price) => {
+    axios.get(`https://api-tutor.herokuapp.com/v1/cars/price/${price}`)
+        .then((response) => {
+            const res = resultsTemplate({"results": response.data});
+            results.innerHTML = res
+        });
+}
+
+const filterByRegNo = (regNo) => {
+    axios.get(`https://api-tutor.herokuapp.com/v1/cars/reg_number/${regNo}`)
+        .then((response) => {
+    
+            const res = resultsTemplate({"results": response.data});
+            results.innerHTML = res
+        });
+}
+
 
 const filterByColor = (color) => {
     axios.get(`https://api-tutor.herokuapp.com/v1/cars/color/${color}`)
@@ -53,6 +72,7 @@ const filterByMakeAndColor = (make, color) => {
     });
 }
 
+
 generateButton.addEventListener("click",() =>{
     if (makeValue.value && colorValue.value ) {
         filterByMakeAndColor(makeValue.value, colorValue.value)
@@ -60,9 +80,12 @@ generateButton.addEventListener("click",() =>{
         filterByMake(makeValue.value)
     }else if(colorValue.value){
         filterByColor(colorValue.value)
+    }else if (reg_numberValue.value) {
+       filterByRegNo(reg_numberValue.value)
     }
 console.log(makeValue.value);
 console.log(colorValue.value);
+console.log(priceValue.value);
 
 })
 
